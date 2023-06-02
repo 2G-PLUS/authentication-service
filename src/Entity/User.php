0a284591c0cc4552638c442ab3ee9013dea1c9b3
armand.deshais <armand.deshais@siqual.fr>
2023-06-02 13:48:42 +0200

    feat(users): routes + login

---
+++ src/Entity/User.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\TimestampableTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Exception;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @method string getUserIdentifier()
+ */
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    use TimestampableTrait;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $password;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private array $roles = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function __call($method, $arguments)
+    {
+        throw new Exception("Invalid method " . $method);
+    }
+}
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     */
+    public function findByEmail(string $email): ?User
+    {
+        return $this->createQueryBuilder('u')
+                    ->andWhere('u.email = :email')
+                    ->setParameter('email', $email)
+                    ->getQuery()
+                    ->getOneOrNullResult();
+    }
+
+} // add your custom repository methods here
